bfcd99c499a134038b3c39cbbfd6b76e96748692
Ashley Buono <81730498+ashleygbuono@users.noreply.github.com>
2022-07-20 14:56:51 -0400

    Time: 101 ms (50.32%), Space: 44.4 MB (24.40%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.js
@@ -11,22 +11,35 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if (!list1 || !list2) return list1 || list2;
-    
-    let merged = new ListNode("x");
+    if (!list1 && !list2) return list1;
+    const merged = new ListNode('x');
     let curr = merged;
+    let l1 = list1, l2 = list2;
     
-    while (list1 && list2) {
-        if (list1.val <= list2.val) {
-            curr.next = list1;
-            list1 = list1.next;
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            curr.next = l1;
+            l1 = l1.next;
         } else {
-            curr.next = list2;
-            list2 = list2.next;
+            curr.next = l2;
+            l2 = l2.next;
         }
         curr = curr.next;
     }
-    curr.next = list1 || list2;
+    
+    if (!l1) curr.next = l2;
+    if (!l2) curr.next = l1;
     
     return merged.next;
 };
+
+/*
+We don't necessarily need to create new nodes for every single input list node, by creating a new list with pointer, we can just save a reference to the nodes in sorted order
+
+
+
+We create a sentinel node first, so we have a next reference to attach the first node reference to
+
+
+
+*/
