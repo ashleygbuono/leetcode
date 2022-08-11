a38832ccebab37a9b944eb22246305abc93a2d9f
Ashley Buono <81730498+ashleygbuono@users.noreply.github.com>
2022-08-11 12:03:49 -0400

    Time: 95 ms (40.38%), Space: 41.8 MB (82.70%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.js
+++ 28-implement-strstr/28-implement-strstr.js
@@ -4,9 +4,13 @@
  * @return {number}
  */
 var strStr = function(haystack, needle) {
+    if (haystack === needle) return 0;
+    
     for (let i = 0; i < haystack.length - needle.length + 1; i++) {
-        let sliced = haystack.slice(i, i + needle.length);
-        if (sliced === needle) return i;
+        if (haystack[i] === needle[0]) {
+            let sliced = haystack.slice(i, i + needle.length);
+            if (sliced === needle) return i;
+        }
     }
     return -1;
 };
@@ -18,5 +22,7 @@ Brute force:
     * Create a slice of haystack, from current i to needle.length
     * If sliced matches needle, return i
 * Return -1
+Time: O(n) where n is haystack length, Space: O(1)
+
 
 */
